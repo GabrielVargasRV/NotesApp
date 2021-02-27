c1d96f50d0a4d21bf4003d0de633bf72d4133e0f
Gabriel Vargas <josegabrielrosariovarg@gmail.com>
2021-02-27 14:55:55 -0400

    notes can be edited

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Note = require("../models/Note");
 
-router.get('/notes', (req,res) => {
-	res.send('Notes from Data Base');
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new-note");
+});
+
+router.post("/notes/new-note", async (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+  if (!title) {
+    errors.push({ text: "Please Write a Title" });
+  }
+  if (!description) {
+    errors.push({ text: "Please Write a Description" });
+  }
+  if (errors.length > 0) {
+    res.render("notes/new-note", {
+      errors,
+      title,
+      description,
+    });
+  } else {
+    const newNote = new Note({ title, description });
+    await newNote.save();
+    res.redirect("/notes");
+  }
+});
+
+router.get("/notes", async (req, res) => {
+  const notes = (await Note.find().lean()).reverse();
+  res.render("notes/all-notes", { notes });
+});
+
+router.get("/notes/edit/:id", async (req, res) => {
+  const note = await Note.findById(req.params.id).lean();
+  res.render("notes/edit-note", { note });
+});
+
+router.put("/notes/edit-note/:id", async (req, res) => {
+  const { title, description } = req.body;
+  // const title = req.body.title;
+  // const description = req.body.description;
+  await Note.findByIdAndUpdate(req.params.id, { title, description });
+  res.redirect("/notes");
 });
 
 module.exports = router;
