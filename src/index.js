a39f20421c07a7dac8fba13f60cbf4af9dc2125e
Gabriel Vargas <josegabrielrosariovarg@gmail.com>
2021-02-27 16:11:35 -0400

    alert messages

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const expHbs = require("express-handlebars");
 const path = require("path");
-const methodOverride = require('method-override');
-const session = require('express-session')
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
 //INITIAZATIONS
 const app = express();
-require('./database')
+require("./database");
 //SETTINGS
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
-app.engine(".hbs",
+app.engine(
+  ".hbs",
   expHbs({
     defaultLayout: "main",
     layoutsDir: path.join(app.get("views"), "layouts"),
@@ -19,21 +21,29 @@ app.engine(".hbs",
 );
 app.set("view engine", ".hbs");
 //MIDDLEWARES
-app.use(express.urlencoded({extended: false}));
-app.use(methodOverride('_method'));
-app.use(session({
-	secret: 'mysecretapp',
-	resave: true,
-	saveUninitialized: true
-}));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(
+  session({
+    secret: "mysecretapp",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 //GLOBAL VARIABLES
+app.use((req,res,next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
 
+  next();
+});
 //ROUTES
-app.use(require('./routes/index.js'));
-app.use(require('./routes/notes.js'));
-app.use(require('./routes/users.js'));
+app.use(require("./routes/index.js"));
+app.use(require("./routes/notes.js"));
+app.use(require("./routes/users.js"));
 //STATIC FILES
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //SERVER IS LISTENNIG
 app.listen(app.get("port"), () => {
   console.log(`SERVER ON PORT: ${app.get("port")}`);
