161e1b6fc199efbca67c56ba12e986bc14c43de9
Gabriel Vargas <josegabrielrosariovarg@gmail.com>
2021-02-27 20:26:00 -0400

    v1

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const path = require("path");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require('passport');
 //INITIAZATIONS
 const app = express();
 require("./database");
+require('./config/passport');
 //SETTINGS
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
@@ -30,12 +32,15 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 //GLOBAL VARIABLES
 app.use((req,res,next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
-
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
   next();
 });
 //ROUTES
